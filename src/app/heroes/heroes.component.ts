9cb3b68cafed584c59dfc2c6c48d9febc6b61fe8
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-11-19 16:20:01 +0100

    add hero service for getting data using observables, message service to broadcast messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
-    name: "Windstorm"
-  }
+    name: 'Windstorm',
+  };
+  heroes: Hero[] = [];
+  selectedHero!: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  onSelectHero(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe((heros) => (this.heroes = heros));
+  }
 }
