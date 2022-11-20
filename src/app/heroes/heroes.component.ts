e7c2503d328b694834750a792ffade67aacceca3
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-11-20 12:33:37 +0100

    add the hero service for updating, log service, error handler

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -10,26 +8,15 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
   heroes: Hero[] = [];
-  selectedHero!: Hero;
 
-  constructor(private heroService: HeroService, private http: HttpClient) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelectHero(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
-    this.heroService
-      .getHeroes()
-      .subscribe((heros) => (this.heroes = heros));
+    this.heroService.getHeroes().subscribe((heros) => (this.heroes = heros));
   }
 }
